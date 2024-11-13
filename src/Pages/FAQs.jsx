86f298a205d7ab3f92b3013b1ae58b035e5ad493
kkarimwahba <karimwahba53@gmail.com>
2024-11-13 16:35:58 +0200

    FAQsPage + Media Query + Changing file names

---
+++ src/Pages/FAQs.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { Box } from "@mui/material";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import MessageForm from "../components/messageForm";
+import FAQsComponent from "../components/FAQs";
+
+function FAQsPage() {
+  return (
+    <Box classname="FAQs-page">
+      <Navbar />
+      <header className="FAQs-hero">
+        <h1>
+          Your questions,<br></br> answered.
+        </h1>
+        <p>
+          If you have any questions not mentioned below, contact us with how we
+          could help you.
+        </p>
+        <button className="FAQs-contact-btn">Contact Us</button>
+      </header>
+      <section className="FAQs-filter-section">
+        <select className="FAQs-filter">
+          <option>All topics</option>
+          <option>All topics</option>
+        </select>
+      </section>
+      <section className="FAQs-content">
+        <FAQsComponent />
+        <FAQsComponent />
+      </section>
+      <section className="FAQs-content">
+        <div className="FAQs-form">
+          <Box>
+            <h2>
+              Ask for anything or <br />
+              just leave us a note.
+            </h2>
+            <p>
+              We'd love to hear from you. If you'd prefer to email us instead,
+              reach <br /> out to{" "}
+              <a href="mailto:hello@travista.com">hello@travista.com</a>.
+            </p>
+          </Box>
+          <Box className="FAQs-form-section">
+            <MessageForm />
+          </Box>
+        </div>
+      </section>
+      <Footer />
+    </Box>
+  );
+}
+export default FAQsPage;
