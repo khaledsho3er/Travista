baa2dc102a04fe0c568e8aeb2e030d6176251f0c
Karim Ahmed Wahba <93930431+kkarimwahba@users.noreply.github.com>
2025-07-21 19:07:22 +0300

    style: Update toolbar options in BlogManagement component to include alignment and remove image option for improved content formatting

--- src/components/Dashboard/BlogManagement.jsx
+++ src/components/Dashboard/BlogManagement.jsx
@@ -262,7 +262,8 @@ const BlogManager = () => {
                     [{ header: [1, 2, 3, false] }],
                     ["bold", "italic", "underline", "strike"],
                     [{ list: "ordered" }, { list: "bullet" }],
-                    ["link", "image"],
+                    [{ align: [] }],
+                    ["link"],
                     ["clean"],
                   ],
                 }}
